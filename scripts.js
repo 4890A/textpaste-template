0bc33df2412a182d95b740aa9d965a621e091bbd
unknown <alexclass2015@gmail.com>
2019-11-25 21:01:12 -0500

    vertical-rl text, limit page scroll

---
+++ scripts.js
@@ -0,0 +1,8 @@
+function count_p() {
+    var pars = document.getElementsByTagName("p");
+    if (pars.length > 6) {
+        pars[0].parentNode.removeChild(pars[0])
+    }
+}
+
+var intervalID = setInterval(count_p, 2000)
